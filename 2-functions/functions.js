90b91b02904b51ff2ac3d28bf474191266411f37
catetile <catetile@gmail.com>
2019-11-29 01:00:41 +0300

    function 25

--- 2-functions/functions.js
+++ 2-functions/functions.js
@@ -683,7 +683,7 @@ describe("functions", function() {
                     return function() {
                         this.doSomething();
                         this.doAnotherThing();
-                    };
+                    }.bind(this);
                 },
                 doSomething: function() {},
                 doAnotherThing: function() {}
